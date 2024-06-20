463afee30a13a3422273bd4b958242bf8f158084
jairolopezlon <31752670+jairolopezlon@users.noreply.github.com>
2024-06-20 18:31:16 -0500

    chore: material for session 2

---
+++ session002/index.js
@@ -0,0 +1,130 @@
+// //*** Comentarios ***
+
+// // Comentario de una sola línea
+// // Este es un comentario de una sola línea
+
+// // Otro comentario de una sola línea
+
+// /*
+// Este es un comentario de varias líneas.
+// Puede abarcar múltiples líneas.
+// */
+
+// /*
+// Otro comentario de varias líneas.
+// Explicación adicional del código.
+// */
+
+// //*** String o Cadenas de Texto ***
+
+// // Comillas dobles
+// let saludo1 = "Hola, Mundo!";
+// console.log(saludo1);
+// alert(saludo1);
+
+// let nombre1 = "Juan";
+// let mensaje1 = "Mi nombre es " + nombre1;
+// console.log(mensaje1);
+// alert(mensaje1);
+
+// // Comillas simples
+// let saludo2 = "Hola, Mundo!";
+// console.log(saludo2);
+// alert(saludo2);
+
+// let nombre2 = "Juan";
+// let mensaje2 = "Mi nombre es " + nombre2;
+// console.log(mensaje2);
+// alert(mensaje2);
+
+// // Comillas invertidas (Template Literals)
+// let nombre3 = "Juan";
+// let saludo3 = `Hola, ${nombre3}!`;
+// console.log(saludo3);
+// alert(saludo3);
+
+// let edad = 30;
+// let mensaje3 = `Tengo ${edad} años.`;
+// console.log(mensaje3);
+// alert(mensaje3);
+
+// //*** Condicional ***
+
+// // if
+// let edadUsuario = prompt("¿Cuál es tu edad?");
+// if (edadUsuario >= 18) {
+//   console.log("Eres mayor de edad.");
+//   alert("Eres mayor de edad.");
+// }
+
+// // if-else
+// if (edadUsuario < 18) {
+//   console.log("Eres menor de edad.");
+//   alert("Eres menor de edad.");
+// } else {
+//   console.log("Eres mayor de edad.");
+//   alert("Eres mayor de edad.");
+// }
+
+// //*** Operadores de Comparación ***
+
+// // ==
+// let a = 5;
+// let b = "5";
+// if (a == b) {
+//   console.log("5 == '5' es verdadero");
+//   alert("5 == '5' es verdadero");
+// }
+
+// // != (más adelante)
+// if (a != 6) {
+//   console.log("5 != 6 es verdadero");
+//   alert("5 != 6 es verdadero");
+// }
+
+// // === (más adelante)
+// if (a === 5) {
+//   console.log("5 === 5 es verdadero");
+//   alert("5 === 5 es verdadero");
+// }
+
+// // !== (más adelante)
+// if (a !== b) {
+//   console.log("5 !== '5' es verdadero");
+//   alert("5 !== '5' es verdadero");
+// }
+
+// //*** Concatenar Texto o Variables ***
+
+// // Signo "+"
+// let saludo4 = "Hola";
+// let nombre4 = "Mundo";
+// let mensaje4 = saludo4 + ", " + nombre4 + "!";
+// console.log(mensaje4);
+// alert(mensaje4);
+
+// let parte1 = "Buenos días, ";
+// let parte2 = "¿cómo estás?";
+// let mensaje5 = parte1 + parte2;
+// console.log(mensaje5);
+// alert(mensaje5);
+
+// // Template string
+// let nombre5 = "Carlos";
+// let saludo5 = `Hola, ${nombre5}! ¿Cómo estás?`;
+// console.log(saludo5);
+// alert(saludo5);
+
+// let ciudad = "Madrid";
+// let mensaje6 = `Vivo en ${ciudad}.`;
+// console.log(mensaje6);
+// alert(mensaje6);
+
+// // Método de String concat
+// let saludo6 = "Hola".concat(", Mundo!");
+// console.log(saludo6);
+// alert(saludo6);
+
+// let mensaje7 = "Buenos".concat(" días, ", "Mundo!");
+// console.log(mensaje7);
+// alert(mensaje7);
